7b65b51be20bab9dd8a4d897f7012fd2989b102f
jkok <jacek.okrojek@gft.com>
2017-02-15 10:55:36 +0100

    Added error codes and sort option

--- README.md
+++ README.md
@@ -5,22 +5,22 @@ Przykłady i ćwiczenia dla mini warsztatu testowania aplikacji z wykokrzystanie
 ## Przygotowanie do warsztatów
 1. Pobierz wymagane narzędzia
   * Zainstaluj przeglądarkę Chrome
-  * Zainstaluj [Node.js](https://nodejs.org/)
+  * Zainstaluj [Node.js](https://nodejs.org/) ( warsztat był tworzony i testowany dla wersji v6.9.5 )
   * Pobierz zawartość repozytorium. Jeśli korzystasz z narzędzia [Git](https://git-scm.com/downloads) sklonuj repozytorium, jeśli nie, naciśnij przycisk `Download ZIP` i rozpakuj pobrane archiwum.
 
-  * Zainstaluj pozostałe pakiety
+2. Zainstaluj pakiety JavaScript
 
     `npm install`
     
-2. Wystartuj testową aplikację  ( do większości ćwiczeń możesz też bez konieczności instalacji korzystać z aplikacji dostępnej na stronie http://jacekokrojek.github.io/jak-to-zrobic-w-js/ )
+3. Wystartuj testową aplikację  ( do większości ćwiczeń możesz też bez konieczności instalacji korzystać z aplikacji dostępnej na stronie http://jacekokrojek.github.io/jak-to-zrobic-w-js/ )
 
     `npm start`
 
-3. Sprawdź poprawność instalacji startując w oddzielnym terminalu przykładowy test
+4. Sprawdź poprawność instalacji startując w oddzielnym terminalu przykładowy test
 
     `protractor excercsises\1_introduction\conf.js`
 
-4. Jeśli nie masz swojego ulubionego edytora JavaScript zainstaluj [Visual Studio Code](https://code.visualstudio.com/) lub [Atom](https://atom.io/)
+5. Jeśli nie masz swojego ulubionego edytora JavaScript zainstaluj [Visual Studio Code](https://code.visualstudio.com/) lub [Atom](https://atom.io/)
 
 **Uwaga:** W przypadku problemów w środowisku Windows zapoznaj się z postem [Nodejs cannot find installed module on Windows](http://stackoverflow.com/questions/9587665/nodejs-cannot-find-installed-module-on-windows/). Jeśli ciągle będziesz miał problem zgłoś go w zakładce `Issues`. Postaram się pomóc.
 
@@ -29,10 +29,29 @@ Przykłady i ćwiczenia dla mini warsztatu testowania aplikacji z wykokrzystanie
 
 ## API
 
-* GET /rest/prices/fb - pobieranie danych o cenach akcji
-* POST /rest/prices - dodawanie danych o cenach akcji. Dane należy przesłać w formacie { "code": <stock-code>, "date" : <data-in-format-YYYY-MM-DD>, "price" : <price> }. Przykład 
+* GET /rest/prices/:code[?sortOrder=(asc|desc)] - pobieranie danych o cenach akcji (aktualnie baza zawiera dane tylko dla codu "fb"). 
 
-`curl -d '{ "code": "fb", "date" : "2017-06-31", "price" : "20.00" }' -H "Content-Type: application/json" http://localhost:3000/rest/prices`
+  Przykład zapytania:
+
+  `curl -H "Content-Type: application/json" http://localhost:3000/rest/prices/fb`
+
+  Error messages: 
+  
+    Not supported stock code
+
+
+* POST /rest/prices - dodawanie danych o cenach akcji. Dane należy przesłać w formacie { "code": <stock-code>, "date" : <data-in-format-YYYY-MM-DD>, "price" : <price> }. 
+
+  Przykład zapytania
+
+  `curl -d '{ "code": "fb", "date" : "2017-06-31", "price" : "20.00" }' -H "Content-Type: application/json" http://localhost:3000/rest/prices`
+
+  Error messages: 
+  
+    Missing stock code
+    Missing price date
+    Not supported date format
+    Missing stock price
 
 ## Dodatkowe informacje
 
--- app.js
+++ app.js
@@ -6,21 +6,35 @@ var sqlite3 = require('sqlite3').verbose();
 
 app.use(express.static('docs'))
 
-app.get('/rest/prices/fb', function (req, res, next) {
-
-    var data = [];
+app.get('/rest/prices/:code', function (req, res, next) {
 
     var db = new sqlite3.Database('app.sqlite3');
-
     db.serialize(function () {
+        
+        var preparedStatement = "SELECT date, price FROM STOCK_PRICES where code = ?";
+        var order = req.query.sortOrder;
+        if (order != null && order != ""){
+            if (order == "asc"){
+                preparedStatement += " order by date asc";
+            }
+            else if (order == "desc"){
+                preparedStatement += " order by date desc";
+            }
+        }
 
-        db.each("SELECT date, price FROM STOCK_PRICES order by date desc",
+        var stockPrices = [];
+        var stockCode = req.params.code;
+        db.each(preparedStatement, stockCode,
             function (err, row) {
-                data.push({ x: row.date, y: row.price });
+                stockPrices.push({ x: row.date, y: row.price });
             }, function (err, count) {
                 db.close();
-                console.log(["data: ", data]);
-                res.send(data);
+                console.log(["data: ", stockPrices]);
+                if ( stockPrices.length ) {
+                    res.send(stockPrices);
+                } else {
+                    res.status(400).send("Not supported stock code");
+                }
             });
 
     });
@@ -30,20 +44,39 @@ app.use(bodyParser.json());
 
 app.post('/rest/prices', function (request, response) {
 
-    console.log("Saving data : " + request.body) // Show the HTML for the Google homepage.    
+    var result = [];
 
-    var db = new sqlite3.Database('app.sqlite3');
-    db.serialize(function () {
-        var stmt = db.prepare("INSERT INTO STOCK_PRICES VALUES (?,?,?)");
-        stmt.run(request.body.code, request.body.date, request.body.price);
-        stmt.finalize();
-    });
-
-    db.close();
-
-    response.send({ result: "OK" });    // echo the result back
-
+    if (!request.body.code) {
+        result.push("Missing stock code ")
+    }
+    if (!request.body.date) {
+        result.push("Missing price date ")
+    }
+    if (request.body.date && isNaN(Date.parse(request.body.date)))
+    {
+        result.push("Not supported date format ")
+    }   
+    if (!request.body.price) {
+        result.push("Missing stock price ")
+    }
+    if (result.length == 0){
+        console.log("Saving data : " + request.body) // Show the HTML for the Google homepage.    
+        var db = new sqlite3.Database('app.sqlite3');
+        db.serialize(function () {
+            var stmt = db.prepare("INSERT INTO STOCK_PRICES VALUES (?,?,?)");
+            stmt.run(request.body.code, request.body.date, request.body.price);
+            stmt.finalize();
+        });
+        db.close();
+        result = ["Stock price added"]
+    }
+    console.log("Sending response : " + result) 
+    response.send(result.join());
 });
+
+app.use(function (err, req, res, next) {
+  res.status(400).send( { result: err.stack })
+})
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
