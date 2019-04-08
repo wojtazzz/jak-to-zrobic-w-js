2610fddd23ee5742f2a132ec9fcb4004db6a1a75
Jaroslaw Kotus <jaroslawkotus@gmail.com>
2019-04-08 12:40:37 +0200

    changes in 1 and 2 excercises

--- excercsises/3_synchronization/contact.spec.js
+++ excercsises/3_synchronization/contact.spec.js
@@ -17,7 +17,7 @@ describe('Protractor Workshop app', function () {
 		const newLocal = 'Your message has been sent.';
 		var expectedElement = contactPage.findElementByText(newLocal, 'h3');
 		browser.wait(EC.visibilityOf(expectedElement), 10000, 'Element still not exist');
-		expect(expectedElement.getText()).toEqual(newLocal);	
+		expect(expectedElement.getText()).toEqual(newLocal);
 
 	});
 
