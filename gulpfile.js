fa5fc2880e03edd7bd501772bd7850a31964cfec
Bartosz <bartosz.przybysz@gmail.com>
2019-04-09 16:57:27 +0200

    gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,38 @@
 var gulp = require('gulp');
 
 gulp.task('elo', function(){
-console.log("yo")
+console.log(process.argv.splice[2])
 });
 
 var protractor = require("gulp-protractor").protractor;
 
+var exc = {
+    '1': "excercsises/2_page_object/home.spec.js",
+    '2': "excercsises/3_synchronization/contact.spec.js",
+    '3': "excercsises/3_synchronization/home.spec.js"
+};
+
+
+
+gulp.task('try', function(){
+    
+    var queue = process.argv.slice(-1);
+
+    gulp.src(["excercsises/*js"])
+    .pipe(protractor({
+        configFile: exc[queue]
+    }))
+    .on('error', function(e) {throw e});
+
+});
+
+gulp.task('before', function(){
+    console.log("start")
+});
+
+
+gulp.task('after',function(){
+    console.log("finish")
+
+});
 
