4006843f3b6e79fdef9f2613e0c77de82a6bee7b
Bartosz <bartosz.przybysz@gmail.com>
2019-04-09 13:11:52 +0200

    var letter change

--- excercsises/gulpfile.js
+++
@@ -1,5 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('siemka', function(){
-    console.log('siemka')
-});
---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+
+gulp.task('elo', function(){
+console.log("yo")
+});
+
+var protractor = require("gulp-protractor").protractor;
+
+
